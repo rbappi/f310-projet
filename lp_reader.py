60b7cd9b6b3ed208e91a41d1da1caf93d9661778
rbappi <73661393+rbappi@users.noreply.github.com>
2023-04-06 11:21:34 +0200

    added docs and some empty python files
    these files are concept file to be filled in

--- lp_reader.py
+++ lp_reader.py
@@ -1,27 +1,42 @@
 #!/usr/bin/python
 from problem_info import ProblemInfo
+import linecache
 
 
 class FileReader():
     def __init__(self, fileName) -> None:
-        self.fileName = fileName
+        self._fileName = fileName
+        self._problemInfo = ProblemInfo()
+        self._readFile()
 
-    def readFile(self):
-        nodes = 0
-        source = 0
-        sink = 99
-        arcs = 961
-        file = open(self.fileName)
-        # for line in range(4):
-        #     if line
+    def _readFile(self):
+        file = open(self._fileName)
+        fileLines = file.readlines()
+        count = 0
+        for line in fileLines:
+            if count == 0:
+                self._problemInfo.nodes = line.strip().split()[1]
+            elif count == 1:
+                self._problemInfo.source = line.strip().split()[1]
+            elif count == 2:
+                self._problemInfo.sink = line.strip().split()[1]
+            elif count == 3:
+                self._problemInfo.arcs = line.strip().split()[1]
+            else:
+                break
+            count += 1
+        file.close()
+        print(f"nodes: {self._problemInfo.nodes}")
+        print(f"source: {self._problemInfo.source}")
+        print(f"sink: {self._problemInfo.sink}")
+        print(f"arcs: {self._problemInfo.arcs}")
+
+
 
 
 def main():
-    probI = ProblemInfo()
-    probI.setAll(0, 0, 99, 961)
-    # probI.arcs = 961
-    print(probI.arcs)
-    print("test")
+    FileReader("instances/inst-100-0.1.txt")
+
 
 
 if __name__ == "__main__":
